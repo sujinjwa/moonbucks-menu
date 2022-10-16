254ad816e6908e87af8017e2bc6aa4b59ee65fb6
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-16 23:55:00 +0900

    [blackcoffee-study#16~22] localStorage에 메뉴 상태 저장하여 관리
    상태&로컬스토리지 개념 이해 / localStorage로 메뉴 추가, 수정, 삭제, 불러오기 기능 구현 완료

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,50 @@
 const $ = (selector) => document.querySelector(selector);
 
+const store = {
+  setLocalStorage(menu) {
+    localStorage.setItem("menu", JSON.stringify(menu));
+  },
+  getLocalStorage() {
+    return JSON.parse(localStorage.getItem("menu"));
+  },
+};
+
 function App() {
+  this.menu = []; // 메뉴이름 -> 상태를 나타내는 변하는 데이터이므로 property로 지정
+  this.init = () => {
+    if (store.getLocalStorage().length >= 1) {
+      this.menu = store.getLocalStorage();
+    }
+
+    showMenuItems(); // menu Items 보여주기
+  };
+
+  const showMenuItems = () => {
+    const menuItemTemplate = this.menu
+      .map((item, index) => {
+        return `<li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+                <span class="w-100 pl-2 menu-name">${item.name}</span>
+                <button
+                  type="button"
+                  class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                >
+                  수정
+                </button>
+                <button
+                  type="button"
+                  class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                >
+                  삭제
+                </button>
+              </li>`;
+      })
+      .join("");
+
+    $("#espresso-menu-list").innerHTML = menuItemTemplate;
+
+    updateMenuCount();
+  };
+
   const updateMenuCount = () => {
     const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
     $(".menu-count").innerText = `총 ${menuCount}개`;
@@ -14,62 +58,40 @@ function App() {
       return;
     }
 
-    // 추가할 새로운 메뉴의 template 생성 함수
-    const menuItemTemplate = (espressoMenuName) => {
-      return `<li class="menu-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-        >
-          수정
-        </button>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-        >
-          삭제
-        </button>
-      </li>`;
-    };
+    this.menu.push({ name: espressoMenuName }); // 객체 담아주기
+    store.setLocalStorage(this.menu); // localStorage에 추가한 객체 저장하기
 
-    // 위 menuItemTemplate 함수 이용하여 생성한 새로운 메뉴의 template을
-    // #espresso-menu-list 요소 안의 가장 마지막 child("beforeend")에
-    // 추가(insertAdjacentHTML)해준다
-    $("#espresso-menu-list").insertAdjacentHTML(
-      "beforeend",
-      menuItemTemplate(espressoMenuName)
-    );
+    showMenuItems();
 
-    // menu-list 내 li item의 총 개수
-    updateMenuCount();
-
-    // 메뉴 추가 후 input값은 빈 값으로 초기화
     $("#espresso-menu-name").value = "";
   };
 
   const updateMenuName = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt(
       "수정할 메뉴명을 입력해주세요: ",
       $menuName.innerText
     );
+    this.menu[menuId].name = updatedMenuName; // HTML 내 보이는 this.menu의 name 수정
+    store.setLocalStorage(this.menu); // 새로 수정된 this.menu를 localSorage에 저장
     $menuName.innerText = updatedMenuName;
   };
 
   const removeMenuName = (e) => {
     if (confirm("정말 삭제하시겠습니까?")) {
+      const menuId = e.target.closest("li").dataset.menuId;
+      this.menu.splice(menuId, 1);
       e.target.closest("li").remove();
+      store.setLocalStorage(this.menu);
       updateMenuCount();
     }
   };
 
-  // form 태그가 자동으로 서버에 데이터 전송하는 것을 막아준다
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
-  // prompt 인터페이스 이용해 메뉴 수정 & 삭제하기
   $("#espresso-menu-list").addEventListener("click", (e) => {
     if (e.target.classList.contains("menu-edit-button")) {
       updateMenuName(e);
@@ -83,10 +105,19 @@ function App() {
 
   $("#espresso-menu-name").addEventListener("keypress", (e) => {
     if (e.key !== "Enter") {
-      return; // 뒷부분 코드가 실행되지 않도록 설정
+      return;
     }
     addMenuName();
   });
+
+  $("nav").addEventListener("click", (e) => {
+    const isCategoryButton = e.target.classList.contains("cafe-category-name");
+    if (isCategoryButton) {
+      const categoryName = e.target.dataset.categoryName;
+      console.log(categoryName);
+    }
+  });
 }
 
-App();
+const app = new App();
+app.init(); // 해당 메소드 실행시켜주기
